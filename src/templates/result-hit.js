9bbf916956f555f0bd0f22d19831238d0aaaa8f9
Josh Goldstein <joshsgoldstein@gmail.com>
2020-12-02 23:59:58 -0500

    more things for the app

--- src/templates/result-hit.js
+++ src/templates/result-hit.js
@@ -1,4 +1,5 @@
 const resultHit = hit => `<div class="result-hit">
+<a href="${hit.url}">
   <div class="result-hit__image-container">
     <img class="result-hit__image" src="${hit.image}" />
   </div>
@@ -6,6 +7,7 @@ const resultHit = hit => `<div class="result-hit">
     <h3 class="result-hit__name">${hit._highlightResult.name.value}</h3>
     <p class="result-hit__price">$${hit.price}</p>
   </div>
+  </a>
 </div>`;
 
 export default resultHit;
